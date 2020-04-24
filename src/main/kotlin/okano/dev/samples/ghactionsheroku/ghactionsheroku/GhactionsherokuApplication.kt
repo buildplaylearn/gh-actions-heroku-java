d9c814c003d7809315505271422ee056f7b8652c
William Johnson dos Santos Okano <william.okano@n26.com>
2020-04-25 01:52:50 +0200

    Initial commit

---
+++ src/main/kotlin/okano/dev/samples/ghactionsheroku/ghactionsheroku/GhactionsherokuApplication.kt
@@ -0,0 +1,11 @@
+package okano.dev.samples.ghactionsheroku.ghactionsheroku
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GhactionsherokuApplication
+
+fun main(args: Array<String>) {
+	runApplication<GhactionsherokuApplication>(*args)
+}
---
+++ src/test/kotlin/okano/dev/samples/ghactionsheroku/ghactionsheroku/GhactionsherokuApplicationTests.kt
@@ -0,0 +1,13 @@
+package okano.dev.samples.ghactionsheroku.ghactionsheroku
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GhactionsherokuApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
