07b4fcc55585f381535eda90ff727b2d4804d322
William Johnson dos Santos Okano <william.okano@n26.com>
2020-04-25 02:43:50 +0200

    DKB-1 added another page

--- src/main/kotlin/okano/dev/samples/ghactionsheroku/ghactionsheroku/controller/HomeController.kt
+++ src/main/kotlin/okano/dev/samples/ghactionsheroku/ghactionsheroku/controller/HomeController.kt
@@ -1,15 +1,26 @@
 package okano.dev.samples.ghactionsheroku.ghactionsheroku.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+
+typealias BasicResponse = Map<String, String>
 
 @RestController("/")
 class HomeController {
 
     @GetMapping
-    fun index(): Map<String, String> =
+    fun index(): BasicResponse =
         mapOf(
             "hello" to "world",
             "timestamp" to System.currentTimeMillis().toString()
+        )
+
+    @GetMapping("/{name}")
+    fun me(@PathVariable name: String): BasicResponse =
+        mapOf(
+            "hello" to "world",
+            "with" to name,
+            "on" to System.currentTimeMillis().toString()
         )
 }
